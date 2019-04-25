45a53af8390a758e59dc17dde51be7cfaeafec40
Jorge <julianacero98@gmail.com>
2019-04-24 20:33:49 -0500

    deploy

---
+++ precache-manifest.1462cc9db463388da47c413db745d973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e71f8a1a7101fda2a9df7bcd40a8725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d1e15e570ed0a9a7c7d",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8d1e15e570ed0a9a7c7d",
+    "url": "/static/js/main.8eba85bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
